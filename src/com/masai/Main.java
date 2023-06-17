138821a7ef403e45e236a4054e3d1a6801cc7595
Manishgupt5 <manishgupt9211@gmail.com>
2023-06-17 19:29:47 +0530

    day5

--- src/com/masai/Main.java
+++ src/com/masai/Main.java
@@ -1,14 +1,19 @@
 package com.masai;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectOutputStream;
+
 import java.util.Map;
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import com.masai.entities.Customer;
 import com.masai.entities.Restaurant;
 import com.masai.exceptions.DuplicateDataException;
+import com.masai.exceptions.InvalidCredentialsException;
+import com.masai.utility.Admin;
 import com.masai.utility.FileExists;
 
 public class Main {
@@ -20,17 +25,19 @@ public class Main {
         try {
             Scanner sc = new Scanner(System.in);
             int preference = 0;
+            System.out.println(" ------------------------------------------------");
+            System.out.println("  Welcome to LocalZomaato Console Application!  ");
+            System.out.println(" ------------------------------------------------");
             do {
-                System.out.println("=================================================");
-                System.out.println("  Welcome to LocalZomaato Console Application!");
-                System.out.println("=================================================");
                 System.out.println("Please select an option to continue:");
                 System.out.println("Who are you !");
                 System.out.println("1. Administrator");
                 System.out.println("2. Customer");
                 System.out.println("3. Restaurant Owner");
                 System.out.println("4. Exit");
+                System.out.println();
                 System.out.print("Enter your choice: ");
+
                 preference = sc.nextInt();
 
                 switch (preference) {
@@ -51,19 +58,24 @@ public class Main {
                 }
 
             } while (preference != 4);
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter a valid number.");
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid selection. Please enter a valid choice.");
+
         } catch (Exception e) {
             e.printStackTrace();
-
         } finally {
             try {
-                ObjectOutputStream poos = new ObjectOutputStream(new FileOutputStream("Restaurant.ser"));
+                ObjectOutputStream poos = new ObjectOutputStream(new FileOutputStream("Restaurants.ser"));
                 poos.writeObject(restaurants);
                 poos.close();
-                ObjectOutputStream coos = new ObjectOutputStream(new FileOutputStream("Customer.ser"));
+                ObjectOutputStream coos = new ObjectOutputStream(new FileOutputStream("Customers.ser"));
                 coos.writeObject(customers);
                 coos.close();
-                // update data
-            } catch (Exception e) {
+            } catch (IOException e) {
+                System.out.println("Error occurred while writing objects to file: " + e.getMessage());
 
                 System.out.println(e.getMessage());
             }
@@ -77,33 +89,111 @@ public class Main {
     private static void customerFunctionality(Scanner sc, Map<String, Customer> customers) {
     }
 
-    private static void adminFunctionality(Scanner sc, Map<String, Restaurant> restaurants) {
-    }
-
-    public static void customerSignup(Scanner sc, Map<String, Customer> customers) throws DuplicateDataException {
-        System.out.println("Enter the following details to Signup");
-
+    private static void adminFunctionality(Scanner sc, Map<String, Restaurant> restaurants)
+            throws InputMismatchException, IllegalArgumentException, InvalidCredentialsException {
+        adminLogin(sc);
         try {
-            System.out.println("Enter the unique username");
+            int adminPreference = 0;
+            System.out.println(" ---------------------------");
+            do {
+                System.out.println("Enter option to continue:");
+                System.out.println("1. Add Restaurant ");
+                System.out.println("2. Delete Restaurant ");
+                System.out.println("3. Print All Restaurant");
+                System.out.println("4. Back");
+                System.out.println();
+                System.out.print("Enter your choice: ");
+
+                adminPreference = sc.nextInt();
+
+                switch (adminPreference) {
+                    case 1:
+                        addRestaurant(sc, restaurants);
+                        break;
+                    case 2:
+                        deleteRestaurant(sc, restaurants);
+                        break;
+                    case 3:
+                        printRestaurant(restaurants);
+                        break;
+                    case 4:
+                        System.out.println("successfully existed from the Admin Page");
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Invalid Selection");
+                }
+            } while (adminPreference != 4);
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter a valid number.");
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid selection. Please enter a valid choice.");
 
         } catch (Exception e) {
-
+            e.printStackTrace();
         }
-        String username = sc.next();
-        System.out.println("Enter the name");
-        String name = sc.next();
-        System.out.println("Enter the password");
-        String pass = sc.next();
-        System.out.println("enter the address");
-        String address = sc.next();
-        System.out.println("Enter the email id");
-        String email = sc.next();
-
-        Customer cus = new Customer(name, username, pass, address, email);
-
-        // CustomerService cusService = new CustomerServiceImpl();
-        // cusService.signUp(cus, customers);
-        System.out.println("customer has Succefully sign up");
 
     }
+
+    public static void printRestaurant(Map<String, Restaurant> restaurants) {
+        System.out.println("______________________________");
+        System.out.println("List of Restaurants:");
+        System.out.println("Total Restaurant : " + restaurants.size());
+        int x = 1;
+        for (Restaurant restaurant : restaurants.values()) {
+            System.out.println("  " + x++ + " : _");
+            System.out.println("Name: " + restaurant.getName());
+            System.out.println("Address: " + restaurant.getAddress());
+            System.out.println("Username/Email : " + restaurant.getEmail());
+            System.out.println("Password : " + restaurant.getPassword());
+
+        }
+        System.out.println("______________________________");
+    }
+
+    public static void deleteRestaurant(Scanner sc, Map<String, Restaurant> restaurants) {
+        System.out.println("Enter Username/Email of Restaurant");
+        String username = sc.next();
+        restaurants.remove(username);
+        System.out.println("Restaurant successfuly deleted");
+    }
+
+    public static void adminLogin(Scanner sc) throws InvalidCredentialsException {
+
+        System.out.println("Enter the username");
+        String userName = sc.next();
+        System.out.println("Enter the password");
+        String password = sc.next();
+        if (userName.equals(Admin.username) && password.equals(Admin.password)) {
+            System.out.println("Welcome back , " + Admin.username);
+        } else {
+            throw new InvalidCredentialsException("Invalid Admin Credentials");
+        }
+    }
+
+    public static void addRestaurant(Scanner sc, Map<String, Restaurant> restaurants) {
+
+        System.out.println("Add a New Restaurant");
+        System.out.print("Enter the restaurant name: ");
+        sc.nextLine();
+        String name = sc.nextLine();
+        System.out.print("Enter the restaurant address: ");
+        String address = sc.next();
+        System.out.println("Enter the restaurant password: ");
+        String password = sc.next();
+        System.out.println("Enter the restaurant Email: ");
+        String email = sc.next();
+        System.out.println("Enter the restaurant opening time: ");
+        String opTime = sc.next();
+        System.out.println("Enter the restaurant closing time: ");
+        String cTime = sc.next();
+
+        Restaurant newRestaurant = new Restaurant(name, password, address, email, opTime, cTime);
+
+        restaurants.put(newRestaurant.getEmail(), newRestaurant);
+
+        System.out.println("Restaurant added successfully!");
+        System.out.println("Logout for updated resturant List");
+    }
+
 }
--- src/com/masai/entities/Customer.java
+++ src/com/masai/entities/Customer.java
@@ -16,12 +16,12 @@ public class Customer extends User {
 
     @Override
     public String toString() {
-        return "Name : " + getName() + ",Username : " + getUsername() + ", Address : " + getAddress() + ", Email : "
+        return "Name : " + getName() + ", Address : " + getAddress() + ", Email : "
                 + getEmail();
     }
 
-    public Customer(String name, String username, String password, String address, String email) {
-        super(name, username, password, address, email);
+    public Customer(String name, String password, String address, String email) {
+        super(name, password, address, email);
     }
 
 }
